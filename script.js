53887880b502000a2743f67eedd4715bba5fa114
Md. Masum Musfique <musfiquemasum8@gmail.com>
2025-02-28 23:19:35 +0600

    layout and clear history btn function added

---
+++ script.js
@@ -0,0 +1,5 @@
+// console.log("Assignment 05");
+
+document.getElementById("clear-history-btn").addEventListener('click', function(){
+    document.getElementById('activityContainer').innerHTML = '';
+})
